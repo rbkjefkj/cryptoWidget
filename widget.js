a3116e5de2ca89f1f719b34514dc7a19007b79d3
rbkjefkj <50465263+rbkjefkj@users.noreply.github.com>
2020-04-22 07:52:15 +0300

    Add files via upload

--- widget.js
+++ widget.js
@@ -1,196 +1,96 @@
 let myChart;
 
 //FUNCTIONS THAT FETCH DATA FOR GRAPHS
-    let prices = [];
-    let bogusData = [100, 100, 100, 91, 72, 53, 44, 41, 22, 17, 11, 5, 14, 23, 35, 41, 49, 58, 47, 14, 54, 33, 100, 92, 100, 100, 100];
-    //let bogusData = [];
-    let timeRN = Date.now();
-    /*fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=EOS&tsym=EUR&limit=90&aggregate=1&toTS=' + timeRN)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            for (let i = 0; i < data.Data.Data.length; i++) {
-               console.log(data.Data.Data[i].open + " " + i);
-                prices.push(data.Data.Data[i].open); 
-            }
-        })
-        .then(function() {*/
-            var ctx = document.getElementById('myChart').getContext('2d'); //instantiates Chart class
-            //var gradient = ctx.createLinearGradient(0, 0, 0, 260);
-            //gradient.addColorStop(0, 'rgba(0, 0, 77, 1)');
-            //gradient.addColorStop(1, 'rgba(0, 0, 77, 0)');    
-
-            let config2 = {
-                type: 'line',
-                data: {
-                labels: bogusData, //prices, //shld be dates
-                datasets: [{
-                    //label: '',
-                    data: bogusData, //prices, //these are the values from API
-                    backgroundColor: 'rgba(77, 77, 255, 0.5)',//gradient, /*[ 'rgba(77, 77, 255, 0.5)' //' 0.8)' //'rgba(248, 148, 6, 0.2)'                    
-                    //'rgba(255, 99, 132, 0.2)',
-                    //'rgba(54, 162, 235, 0.2)',
-                //'rgba(255, 206, 86, 0.2)',
-                //'rgba(75, 192, 192, 0.2)',
-                //'rgba(153, 102, 255, 0.2)',
-                //'rgba(255, 159, 64, 0.2)'
-                //],
-                borderColor: [ 'rgba(0, 0, 77, 0.2)' //'rgba(248, 148, 6, 1)'
-                //'rgba(255, 99, 132, 1)',
-                //'rgba(54, 162, 235, 1)',
-                //'rgba(255, 206, 86, 1)',
-                //'rgba(75, 192, 192, 1)',
-                //'rgba(153, 102, 255, 1)',
-                //'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
-                pointStyle: 'rectRot',
-                pointRadius: 0,
-                lineTension: 0
-            }]
-        },
-    options: {
-        legend: {
-            position: 'right',
-            display: false
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    drawOnChartArea: false
-                },
-                ticks: { maxRotation: 0, autoSkip: true, autoSkipPadding: 30, labelOffset: 7, fontColor: '#b3b3b3' }
-            }],
-            yAxes: [{
-                gridLines: {
-                    drawOnChartArea: false
-                },   
-                display: false,
-                scaleLabel: { display: false }, 
-                ticks: {
-                    beginAtZero: false
-                }
-            }]
-        }
-    }
-}
-
-
-myChart = new Chart(ctx, config2);
-
-//CHART THAT APPEARS WHEN API REQUEST FAILS 
-    /*let failedChart;
-    let failedData = [];
-        var ctx = document.getElementById('myChart').getContext('2d');
-            let config = {
-                type: 'line',
-                data: {
-                labels: failedData,
-                datasets: [{
-                    data: failedData,
-                    backgroundColor:  'rgba(0, 0, 0, 0.1)',
-                borderColor: [ 'rgba(0, 0, 77, 0.2)'
-                ],
-                borderWidth: 1,
-                pointStyle: 'rectRot',
-                pointRadius: 0,
-                lineTension: 0
-            }]
-        },
-    options: {
-        legend: {
-            position: 'right',
-            display: false
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    drawOnChartArea: false
-                },
-                ticks: { display: true, maxRotation: 0, autoSkip: true, autoSkipPadding: 30, labelOffset: 7, fontColor: 'white' }
-            }],
-            yAxes: [{
-                gridLines: {
-                    drawOnChartArea: false
-                },   
-                display: false,
-                scaleLabel: { display: false }, 
-                ticks: {
-                    beginAtZero: false
-                }
-            }]
-        }
-    }
-}
-
-
-myChart = new Chart(ctx, config);*/
-
-
-
-
-//setTimeout(function(){ var myChart = new Chart(ctx, config); }, 100);
-
-
-/*function isTrue() { 
-    let screenWidth = getWidth();
-    if (screenWidth > 520) { above520 = true; return true; } 
-    else { above520 = false; return false; } 
-}
-function getWidth() { 
-    return window.innerWidth;
-    
-}*/
-
-/*function somefunc() {
-    let w = getWidth();
-    if (above520) { //screen is currently big
-        console.log('somefunc ran. Width is above 520')
-        if (w <= 520) {
-            above520 = false;
-            myChart = new Chart(ctx, config2);
-        }
-    } else { //screen is currently small
-        if (w > 520) {
-            console.log('somefunc ran. Width is below 520.');
-            above520 = true;
-            myChart = new Chart(ctx, config);
-        }
-    }
-}
-window.onresize = somefunc;*/
-
-
-
-
+//let bogusData = [100, 100, 100, 91, 72, 53, 44, 41, 22, 17, 11, 5, 14, 23, 35, 41, 49, 58, 47, 14, 54, 33, 100, 92, 100, 100, 100];
+let timeRN = Date.now();
 let fiat = 'EUR';
 let crypto = 'EOS';
 let isCalculator = false;
 
 //CALLED ONLOAD
+let prices = [];
+let dates = [];
 getCurrentPrice2('EUR');
 
 
 function getCurrentPrice2(fiat) {
-        fetch('https://min-api.cryptocompare.com/data/price?fsym=' + crypto + '&tsyms=' + fiat + '&api_key=fd444f02fd13f67cbbbfe6bf9279aa08ca21b36fb99bf2c964790c5b645aa766')
+        fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=' + crypto + '&tsym=' + fiat + '&limit=90&aggregate=1&toTS=' + timeRN)
             .then((resp) => resp.json())
             .catch (err => console.log('caught error;'))
             .then(function(data) {
-                let price;   
+            	console.log(data);	
+            	//let prices = [];
+            	//let dates = [];
+            	data.Data.Data
+            	for (let piece of data.Data.Data) {
+            		prices.push(piece.close);
+            		dates.push(piece.time); // ADD SOME METHOD THAT TURNS THAT INTO AN ACTUAL HUMAN READABLE DATE
+            	} 
+                let price = prices[prices.length-1].toFixed(2);   
                 styleCryptoPrice(data); 
                 try {
-                    fiat === 'EUR' ? price = data.EUR.toFixed(2) : price = data.USD.toFixed(2); //chooses price depending on which FIAT is used     
+                    //fiat === 'EUR' ? price = data.EUR.toFixed(2) : price = data.USD.toFixed(2); //chooses price depending on which FIAT is used     
                     document.getElementById('span').innerHTML = price;                          //paints price
                     fiat === 'EUR' ? document.getElementById('symbol').textContent = '€' : document.getElementById('symbol').textContent = '$' //sets €/$ in title
                     document.getElementById('cryptoName').textContent = crypto;                           //sets crypto's name in title
             
                     if (isCalculator === true) stopCalculator();
-                    document.getElementById('frst-input').value = 'from'  + crypto;             //next 2 lines give text 'from CRYPTO to FIAT'
+                    document.getElementById('frst-input').value = 'from '  + crypto;             //next 2 lines give text 'from CRYPTO to FIAT'
                     document.getElementById('scnd-input').value = 'to ' + fiat; 
-                } catch {
+                } catch(err) {
                     document.getElementById('failedAPI').style.display = "block";    
                 }
-            });
-}
+            })
+            .then(function() {
+            	var ctx = document.getElementById('myChart').getContext('2d'); //instantiates Chart class
+            	
+            	//HOW TO MAKE A GRADIENT
+            	//var gradient = ctx.createLinearGradient(0, 0, 0, 260);
+            	//gradient.addColorStop(0, 'rgba(0, 0, 77, 1)');
+            	//gradient.addColorStop(1, 'rgba(0, 0, 77, 0)');    
+
+            	let config2 = {
+                	type: 'line',
+                	data: {
+                		labels: dates, //prices, //shld be dates
+                		datasets: [{
+                    		//label: '',
+                    		data: prices, //prices, //these are the values from API
+                    		backgroundColor: 'rgba(77, 77, 255, 0.5)',
+                			borderColor: [ 'rgba(0, 0, 77, 0.2)' ],
+                			borderWidth: 1,
+                			pointStyle: 'rectRot',
+                			pointRadius: 0,
+                			lineTension: 0
+            			}]
+        			},
+    				options: {
+        				legend: {
+            				position: 'right',
+            				display: false
+        				},
+        				scales: {
+            				xAxes: [{
+                				gridLines: { drawOnChartArea: false },
+                				ticks: { display: false, maxRotation: 0, autoSkip: true, autoSkipPadding: 30, labelOffset: 7, fontColor: '#b3b3b3' }
+            				}],
+            				yAxes: [{
+                				gridLines: { drawOnChartArea: false },   
+                				display: false,
+                				scaleLabel: { display: false }, 
+                				ticks: {
+                    				beginAtZero: false
+                				}
+            				}]
+        				}
+    				}
+				}//config2
+
+
+				myChart = new Chart(ctx, config2);
+			});//canvas drawing function
+}//the entire func
+
 
 function styleCryptoPrice(data) {
     let stringifiedPrice = JSON.stringify(data);
@@ -322,4 +222,6 @@ function toBTC() {
 
 
 
-//DON'T DELETE:    https://min-api.cryptocompare.com/data/v2/histoday?fsym=EOS&tsym=EUR&limit=90&aggregate=1&toTS=TIMEstampRIGHTNOW
+//DON'T DELETE:    https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=EUR&limit=90&aggregate=1&toTS=TIMEstampRIGHTNOW
+//DON'T DELETE: RETURNS PRICE RN FOR BTC/EOS IN EUR/USD:
+//'https://min-api.cryptocompare.com/data/price?fsym=' + crypto + '&tsyms=' + fiat + '&api_key=fd444f02fd13f67cbbbfe6bf9279aa08ca21b36fb99bf2c964790c5b645aa766'
