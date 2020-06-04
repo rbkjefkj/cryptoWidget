1713ad4294a92ac94617dc018423650f1c2c0e77
rbkjefkj <50465263+rbkjefkj@users.noreply.github.com>
2020-06-04 15:27:01 +0300

    prettified

--- widget.js
+++ widget.js
@@ -1,15 +1,14 @@
 let myChart;
 
 //FUNCTIONS THAT FETCH DATA FOR GRAPHS
-//let bogusData = [100, 100, 100, 91, 72, 53, 44, 41, 22, 17, 11, 5, 14, 23, 35, 41, 49, 58, 47, 14, 54, 33, 100, 92, 100, 100, 100];
 function getTime() {
-  return Date.now();
+	return Date.now();
 }
-
+	
 let fiat = 'EUR';
 let crypto = 'EOS';
 let isCalculator = false;
-
+	
 //CALLED ONLOAD
 getCurrentPrice2('EUR');
 
@@ -20,39 +19,38 @@ function makeDateForHumans(time) {
 }
 
 function getCurrentPrice2(fiat) {
-        let prices = [];
-        let dates = [];
-        let timeRN = getTime();
-        fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=' + crypto + '&tsym=' + fiat + '&limit=90&aggregate=1&toTS=' + timeRN)
-            .then((res) => res.json())
-            .then(function(data) {
-            	console.log(data);
-            	data.Data.Data
-				let ninetyDaysAgo = timeRN - 7776000000; //makes timestamp for the day that was 90 days ago
-				console.log(new Date(ninetyDaysAgo));
-            	for (let piece of data.Data.Data) {
-            	     prices.push(piece.close.toFixed(2));
-					 console.log(makeDateForHumans(ninetyDaysAgo));
-            		 dates.push(makeDateForHumans(ninetyDaysAgo));
-					 ninetyDaysAgo = ninetyDaysAgo + 86400000; //adds the next day
-					 console.log('90 with a day added: ' + ninetyDaysAgo);
-            	}
-              let price = prices[prices.length-1];
-              styleCryptoPrice(data);
-              try {
-                    //fiat === 'EUR' ? price = data.EUR.toFixed(2) : price = data.USD.toFixed(2); //chooses price depending on which FIAT is used
-                    document.getElementById('span').textContent = price;                          //paints price
-                    fiat === 'EUR' ? document.getElementById('symbol').textContent = '€' : document.getElementById('symbol').textContent = '$' //sets €/$ in title
-                    document.getElementById('cryptoName').textContent = crypto;                           //sets crypto's name in title
+	let prices = [];
+    let dates = [];
+    let timeRN = getTime();
+    fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=' + crypto + '&tsym=' + fiat + '&limit=90&aggregate=1&toTS=' + timeRN)
+    	.then((res) => res.json())
+        .then(function(data) {
+           	//data.Data.Data
+			let ninetyDaysAgo = timeRN - 7776000000; //makes timestamp for the day that was 90 days ago
+			console.log(new Date(ninetyDaysAgo));
+            for (let piece of data.Data.Data) {
+           		prices.push(piece.close.toFixed(2));
+				console.log(makeDateForHumans(ninetyDaysAgo));
+            	dates.push(makeDateForHumans(ninetyDaysAgo));
+				ninetyDaysAgo = ninetyDaysAgo + 86400000; //adds the next day
+				console.log('90 with a day added: ' + ninetyDaysAgo);
+            }
+            let price = prices[prices.length-1];
+            styleCryptoPrice(data);
+            try {
+            	//fiat === 'EUR' ? price = data.EUR.toFixed(2) : price = data.USD.toFixed(2); //chooses price depending on which FIAT is used
+                document.getElementById('span').textContent = price;                          //paints price
+                fiat === 'EUR' ? document.getElementById('symbol').textContent = '€' : document.getElementById('symbol').textContent = '$' //sets €/$ in title
+                document.getElementById('cryptoName').textContent = crypto;                           //sets crypto's name in title
 
-                    if (isCalculator === true) stopCalculator();
-                    document.getElementById('frst-input').value = 'from '  + crypto;             //next 2 lines give text 'from CRYPTO to FIAT'
-                    document.getElementById('scnd-input').value = 'to ' + fiat;
-                } catch(err) {
-                    document.getElementById('failedAPI').style.display = "block";
-                }
-            })
-            .then(function() {
+                if (isCalculator === true) stopCalculator();
+                document.getElementById('frst-input').value = 'from '  + crypto;             //next 2 lines give text 'from CRYPTO to FIAT'
+                document.getElementById('scnd-input').value = 'to ' + fiat;
+           	} catch(err) {
+            	document.getElementById('failedAPI').style.display = "block";
+            }
+        })
+        	.then(function() {
             	var ctx = document.getElementById('myChart').getContext('2d'); //instantiates Chart class
 
             	//HOW TO MAKE A GRADIENT
@@ -63,26 +61,23 @@ function getCurrentPrice2(fiat) {
             	let config2 = {
                 	type: 'line',
                 	data: {
-                		labels: dates, //prices, //shld be dates
+                		labels: dates,
                 		datasets: [{
                     		//label: '',
-                    		data: prices, //prices, //these are the values from API
+                    		data: prices, //these are the values from API
                     		backgroundColor: 'rgba(77, 77, 255, 0.5)',
                 			borderColor: [ 'rgba(0, 0, 77, 0.2)' ],
                 			borderWidth: 1,
                 			pointStyle: 'star',
-                			pointRadius: 2,
-                      pointBorderColor: 'rgba(77, 77, 255, 0.5)',
+                			pointRadius: 3,
+                      		pointBorderColor: 'rgba(77, 77, 255, 0.5)',
                 			lineTension: 0
             			}]
-        			 },
-    				      options: {
-        				  legend: {
-            			     position: 'right',
-            				   display: false
-        				  },
-        				  scales: {
-            				  xAxes: [{
+        			},
+    				options: {
+        				legend: { position: 'right', display: false },
+        				scales: {
+            				xAxes: [{
                 				gridLines: { drawOnChartArea: false, drawTicks: false },
                 				ticks: { display: false, maxRotation: 0, autoSkip: true, autoSkipPadding: 30, labelOffset: 7, fontColor: "white"/*'#b3b3b3'*/ }
             				}],
@@ -90,15 +85,11 @@ function getCurrentPrice2(fiat) {
                 				gridLines: { drawOnChartArea: false },
                 				display: false,
                 				scaleLabel: { display: false },
-                				ticks: {
-                            display: false
-                    				//beginAtZero: false
-                				}
+                				ticks: { display: false }
             				}]
         				}
     				}
-				}//config2
-
+				}// end of config2
 
 				myChart = new Chart(ctx, config2);
 			})//canvas drawing function ends
@@ -142,7 +133,7 @@ function styleCryptoPrice(data) {
 
 
 function stopCalculator() {
-     isCalculator = false;
+    isCalculator = false;
     //Removing CSS
     let spans = document.getElementsByClassName('form-spans');
     let inputs = document.getElementsByClassName('inputs');
@@ -179,6 +170,7 @@ function toCalculator() {
     for (let i = 0; i < spans.length; i++) {
         spans[i].style.paddingLeft = '13px';
     }
+	
     //Actually changing text
     console.log(crypto);    //crypto is undefined if you pass it as arg to function toCalculator();
     let cryptoNode = document.createTextNode(crypto);
@@ -197,25 +189,24 @@ function toCalculator() {
 
 
 function update(userInput) {
-        let inFIAT = (((document.getElementById('span').innerHTML * 100) * (userInput * 100))/10000).toFixed(2); // inFIAT = current price * user input
-        document.getElementById("scnd-input").value = inFIAT;  //assigns that to output
-    }
+	let inFIAT = (((document.getElementById('span').innerHTML * 100) * (userInput * 100))/10000).toFixed(2); // inFIAT = current price * user input
+    document.getElementById("scnd-input").value = inFIAT;  //assigns that to output
+}
 
-    let firstInput = document.getElementById("frst-input");
-    firstInput.oninput =  function() {
-        let rex = /\./;
-        let str = firstInput.value.toString();
-        if (str.match(rex) === null) {      //if user types an integer
-            if (firstInput.value.length > 14) firstInput.value = firstInput.value.slice(0, 14);
-        } else firstInput.value = firstInput.value.slice(0, 18);
+let firstInput = document.getElementById("frst-input");
+firstInput.oninput =  function() {
+	let rex = /\./;
+    let str = firstInput.value.toString();
+    if (str.match(rex) === null) {      //if user types an integer
+    	if (firstInput.value.length > 14) firstInput.value = firstInput.value.slice(0, 14);
+    } else firstInput.value = firstInput.value.slice(0, 18);
 
-        let userInput = parseFloat(document.getElementById("frst-input").value);
-        update(userInput);
-    };
-
+    let userInput = parseFloat(document.getElementById("frst-input").value);
+    update(userInput);
+};
 
 function toUSD() {
-    fiat = 'USD';
+	fiat = 'USD';
     getCurrentPrice2('USD');
     //changeFIATsymbols('USD');
 }
@@ -238,9 +229,4 @@ function toBTC() {
     //changeFIATsymbols('EUR');
 }
 
-
-
-
-//DON'T DELETE:    https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=EUR&limit=90&aggregate=1&toTS=TIMEstampRIGHTNOW
-//DON'T DELETE: RETURNS PRICE RN FOR BTC/EOS IN EUR/USD:
-//'https://min-api.cryptocompare.com/data/price?fsym=' + crypto + '&tsyms=' + fiat + '&api_key=fd444f02fd13f67cbbbfe6bf9279aa08ca21b36fb99bf2c964790c5b645aa766'
+//TOP SECRET: 'https://min-api.cryptocompare.com/data/price?fsym=' + crypto + '&tsyms=' + fiat + '&api_key=fd444f02fd13f67cbbbfe6bf9279aa08ca21b36fb99bf2c964790c5b645aa766'
